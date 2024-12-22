39c2fe95cc2ced4ea244e1a875aab56bd7246338
Vanshika Rathi <vanshikarathi2739@gmail.com>
2024-12-22 11:18:34 +0530

    Added namespace user contribution

---
+++ pywikisource/constant.py
@@ -0,0 +1,50 @@
+namespace_data = {
+    "as": {
+        "page": 104,
+        "index": 106
+    },
+    "bn": {
+        "page": 104,
+        "index": 102
+    },
+    "gu": {
+        "page": 104,
+        "index": 106
+    },
+    "hi": {
+        "page": 250,
+        "index": 252
+    },
+    "kn": {
+        "page": 104,
+        "index": 106
+    },
+    "ml": {
+        "page": 106,
+        "index": 104
+    },
+    "mr": {
+        "page": 104,
+        "index": 106
+    },
+    "or": {
+        "page": 250,
+        "index": 252
+    },
+    "pa": {
+        "page": 250,
+        "index": 252
+    },
+    "sa": {
+        "page": 104,
+        "index": 106
+    },
+    "ta": {
+        "page": 250,
+        "index": 252
+    },
+    "te": {
+        "page": 104,
+        "index": 106
+    }
+}
